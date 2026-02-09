3752f7613b76217a40ffb277b6a5d3769de91126
Ryan <rfrost096@gmail.com>
2026-02-09 15:15:19 -0500

    Refactored with new args and new env file

--- dataset22/process.py
+++ dataset22/process.py
@@ -1,8 +1,11 @@
 import pandas as pd
-import yaml
 from pathlib import Path
-from typing import Any
 import plotly.express as px  # type: ignore
+import argparse
+import os
+from dotenv import load_dotenv
+
+DEFAULT_GRAPH = "rsrp"
 
 script_dir = Path(__file__).resolve().parent
 
@@ -32,32 +35,57 @@ dtype_map_dataset_22 = {
     "companion_abs_time_readable": "object",
 }
 
-config: Any = None
 
-with open(script_dir / "../config.yaml", "r") as file:
-    config = yaml.safe_load(file)
+def plot_rsrp(df: pd.DataFrame):
 
-df = pd.read_csv(  # type: ignore
-    config["dataset22"]["path"] + config["dataset22"]["logs"],
-    dtype=dtype_map_dataset_22,  # type: ignore
-    na_values=["Unavailable"],
-    parse_dates=["companion_abs_time_readable"],
-    engine="pyarrow",
-)
+    plot_df = df.dropna(subset=["latitude", "longitude", "altitude", "rsrp"])  # type: ignore
 
-plot_df = df.dropna(subset=["latitude", "longitude", "altitude", "rsrp"])  # type: ignore
+    fig = px.scatter_3d(  # type: ignore
+        plot_df,
+        x="longitude",
+        y="latitude",
+        z="altitude",
+        color="rsrp",
+        color_continuous_scale="Viridis",
+        title="3D Spatial Distribution of RSRP Signal Strength",
+        hover_data=["technology", "bands", "pci"],
+    )
 
-fig = px.scatter_3d(  # type: ignore
-    plot_df,
-    x="longitude",
-    y="latitude",
-    z="altitude",
-    color="rsrp",
-    color_continuous_scale="Viridis",
-    title="3D Spatial Distribution of RSRP Signal Strength",
-    hover_data=["technology", "bands", "pci"],
-)
+    fig.update_traces(marker=dict(size=3))  # type: ignore
 
-fig.update_traces(marker=dict(size=3))  # type: ignore
+    fig.show()  # type: ignore
 
-fig.show()  # type: ignore
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Generate KML file of the specified KPI and cell PCI"
+    )
+    parser.add_argument(
+        "-g",
+        "--graph-name",
+        type=str,
+        default=DEFAULT_GRAPH,
+        help="Graph name. Default is " + DEFAULT_GRAPH,
+    )
+    options = parser.parse_args()
+
+    load_dotenv(script_dir / "../config.env")
+
+    data_path = (
+        str(os.getenv("DATASET_22_HOME"))
+        + "/"
+        + str(os.getenv("DATASET_22_LOGS"))
+        + "/"
+        + str(os.getenv("DATASET_22_FILE"))
+    )
+
+    df = pd.read_csv(  # type: ignore
+        data_path,
+        dtype=dtype_map_dataset_22,  # type: ignore
+        na_values=["Unavailable"],
+        parse_dates=["companion_abs_time_readable"],
+        engine="pyarrow",
+    )
+
+    if options.graph_name == "rsrp":
+        plot_rsrp(df)
