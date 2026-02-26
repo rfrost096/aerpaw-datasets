28545a723cf596b3308761c28f29e4b0545cd671
Ryan <rfrost096@gmail.com>
2026-02-26 09:00:57 -0500

    removed some archive files

--- scripts/analyze_features_22.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-INIT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-source "$INIT_DIR/init.sh"
-
-echo "Analyzing features for dataset 22..."
-analyze-features -d 22 -f lte.csv -g mutual --fields rsrp,rsrq,rssi,asu,pci,ta,ci,longitude,latitude,altitude
